8de837c86756a7a34e26ca295588589faad48a44
Julian <julian_sunpacyo@dlsu.edu.ph>
2022-11-19 13:22:33 +0800

    Syntax

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-import express from 'express';
+const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
