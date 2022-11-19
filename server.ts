8c77fd709e276284e20168f25837c045ccb1d196
Julian <julian_sunpacyo@dlsu.edu.ph>
2022-11-19 12:50:16 +0800

    Require routes

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-
+const routes = require('./routes');
 const dbo = require('./dbConn.ts');
 
 const connectionString = process.env.mongoURI;
@@ -16,10 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(require('./routes/hero_status.ts'));
 
-app.use(function (err, res, _req, next) {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
+app.use('/', routes);
 
 dbo.connectToServer(function (err) {
   if (err) {
